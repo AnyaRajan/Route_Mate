f3c3cefbdd47ba158024e99420df16c751399a87
Swayyyy <spradhan22@iitk.ac.in>
2024-03-15 00:09:15 +0530

    pradhanji

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import Plus from './plus';
 import Formed from './Components/Pankaj/Formed';
 import Index from './Components/Train Details';
 import Blogpage from './Components/Blogpage/Blogpage';
-
 const router = createBrowserRouter([
+  
   { path: "/", element: <Homepage /> },
   { path: "/login", element: <Loginpage /> },
   { path: "/register", element: <Register /> },
@@ -27,8 +27,8 @@ function App() {
 
   return (
     <>
-      {/* <RouterProvider router={router} /> */}
       <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
       
     </>
   );
--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { ReactDOM } from 'react'
 import logo from '../../Assets/logo1.png'
-import rt1 from '../../Assets/rt1.png'
-import lt1 from '../../Assets/lt1.png'
+import rt1 from '../../Assets/rt2.png'
+import lt1 from '../../Assets/lt2.png'
 import girl from '../../Assets/girl.png'
 import boy from '../../Assets/boy.png'
 import trvl from '../../Assets/trvl.jpg'
 import Navbar from './Navbar'
-//import {gsap} from 'gsap'
 import "./Homepage.css"
-
+import Preloader from './Preloader/preloader'
+import './script'
+import 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js'
 function Homepage() {
-  //gsap.to(".box", { rotation: 27, x: 100, duration: 1 });
-
+  const [loading,setloading] = useState(true);
+  useEffect(()=>{
+    setTimeout(() => {
+    setloading(false);
+    },5000)
+  })
   return (
+    loading ? <Preloader/>:
     <>
     <div>
+    <div >
       <Navbar />
     </div>
     <div className="Homepage-container pt-5  bg-center bg-cover" >
@@ -22,10 +30,10 @@ function Homepage() {
       <div className=" w-[40em]" >
     <img src={logo} className="logo w-1/5 pl-10" alt="Logo"/>
   </div>
-        <h1 className='rm text-6xl text-black text-center font-serif pt-10'>Routemate</h1>
+        <h1 className=' text-6xl text-white text-center font-serif pt-10'>Routemate</h1>
       </div>
       <div className=" ">
-      <div class="rm spin flex h-screen pt-10  w-full  justify-center items-baseline">
+      <div class=" spin flex h-screen pt-10  w-full  justify-center items-baseline">
       <div >
     <img src={lt1} alt="leftarrow"/>
   </div>  
@@ -43,18 +51,18 @@ function Homepage() {
     <img src={girl} className="fade-in w-4/5 pl-10" alt="girlimg"/>
   </div>
   </div>
-  <h1 className='text-4xl -mt-20 pb-52 text-black font-serif text-center'>Find travel buddies and discover the world together</h1>
+  <h1 className='text-4xl -mt-20 pb-52 text-white font-serif text-center'>Find travel buddies and discover the world together</h1>
   </div>
   <div className="flex pt-20 pl-10 pr-20 pb-10 w-full flex-row justify-around">
         <img src = {trvl} className="w-[25em] pr-10" alt="travel"/>
-        <div className="flex flex-col text-black text-justify text-base">
+        <div className="flex flex-col text-white text-justify text-base">
           <p className="text-bold text-5xl pb-20 mr-[2em]">About Us</p>
           <p className='text-xl mr-[2em]'>Welcome to Route Mate, your trusted travel companion for shared journeys to your Homepagetown and beyond. Connect with fellow students from IIT Kanpur and embark on safe, enjoyable trips together. Our platform features a chat function, allowing you to communicate with potential travel companions and plan your trip seamlessly. </p>
           <p className='pt-4 text-xl mr-[2em]'>Additionally, explore our blog section for travel tips, destination guides, and inspiring stories. Want to share your own experiences? Create your own blogs and inspire others with your adventures. Join our community today and travel with confidence.</p>
         </div>
       </div>      
   </div>
-  
+  </div>
          
  </>
  )
